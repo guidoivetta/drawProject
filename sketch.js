7d76615b78c0509d137256185c604bb680b383f4
Guido Ivetta <guidoivetta@gmail.com>
2019-06-22 14:24:02 -0300

    no background and color oscillation!

--- sketch.js
+++ sketch.js
@@ -1,26 +1,42 @@
 let shapeSize = 100;
 let growing = true;
+let growthSpeed = 3;
+let color = 255;
+let darkening = true;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight * 0.8);
+  background(51);
+
+  var resetButton = createButton("reset");
+  resetButton.mousePressed(resetSketch);
 }
 
 function draw() {
-  background(51);
-
+  fill(color);
   ellipse(mouseX, mouseY, shapeSize, shapeSize);
 
   if (mouseIsPressed) {
+    //Size oscillation---------------------------
     // checking whether it should grow
     if (shapeSize >= width / 2 || shapeSize <= 50) {
       growing = !growing;
     }
 
     // growing or shrinking
-    shapeSize = growing ? shapeSize + 3 : shapeSize - 3;
+    shapeSize += growing ? growthSpeed : -growthSpeed;
+    
+    
+    //Color oscillation--------------------------
+    if (color == 255 || color == 0) {
+      darkening = !darkening;
+    }
+
+    // growing or shrinking
+    color += darkening ? 1 : -1;
   }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function resetSketch() {
+  background(51);
 }
