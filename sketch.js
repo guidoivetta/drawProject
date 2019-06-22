d8dbbfaec98c1f9a59e0396e13afad0419d7e671
Guido Ivetta <guidoivetta@gmail.com>
2019-06-22 14:33:37 -0300

    fixed window size

--- sketch.js
+++ sketch.js
@@ -5,11 +5,8 @@ let color = 255;
 let darkening = true;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight * 0.8);
+  createCanvas(windowWidth, windowHeight * 0.995);
   background(51);
-
-  // var resetButton = createButton("reset");
-  // resetButton.mousePressed(resetSketch);
 }
 
 function draw() {
@@ -25,9 +22,9 @@ function draw() {
 
     // growing or shrinking
     shapeSize += growing ? growthSpeed : -growthSpeed;
-    
-    
+
     //Color oscillation--------------------------
+    // checking whether it should grow
     if (color == 255 || color == 0) {
       darkening = !darkening;
     }
@@ -35,8 +32,4 @@ function draw() {
     // growing or shrinking
     color += darkening ? 1 : -1;
   }
-}
-
-function resetSketch() {
-  background(51);
 }
