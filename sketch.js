750458f230f02f9620f9cac7d7d288bd5044e547
Guido Ivetta <guidoivetta@gmail.com>
2019-06-25 00:09:49 -0300

    new version!

--- sketch.js
+++ sketch.js
@@ -2,19 +2,27 @@ let shapeSize = 100;
 let growing = true;
 let growthSpeed = 3;
 let color = 255;
+let red = 255;
+let randomGreen;
+let randomBlue;
 let darkening = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight * 0.995);
   background(51);
+  noStroke();
+  
+  randomGreen = random(256);
+  randomBlue = random(256);
 }
 
 function draw() {
-  fill(color);
+  fill(color, randomGreen, randomBlue);
+
   ellipse(mouseX, mouseY, shapeSize, shapeSize);
 
   if (mouseIsPressed) {
-    //Size oscillation---------------------------
+    // Size oscillation---------------------------
     // checking whether it should grow
     if (shapeSize >= width / 2 || shapeSize <= 50) {
       growing = !growing;
@@ -23,13 +31,13 @@ function draw() {
     // growing or shrinking
     shapeSize += growing ? growthSpeed : -growthSpeed;
 
-    //Color oscillation--------------------------
-    // checking whether it should grow
-    if (color == 255 || color == 0) {
+    // Color oscillation--------------------------
+    // checking whether it should darken
+    if (red == 255 || red == 0) {
       darkening = !darkening;
     }
 
-    // growing or shrinking
-    color += darkening ? 1 : -1;
+    // brightening or darkening
+    red += darkening ? 1 : -1;
   }
 }
